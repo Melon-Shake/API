f4ededc9c46c0073ad3fafbb37b76974b2ed675b
iamyanghee <yanghee.data@gmail.com>
2023-08-24 19:29:59 +0900

    route /authorize 확인 완료 (리다이렉션으로)

--- load_spotify.py
+++ load_spotify.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hi"
+    return redirect(url_for("authorize"),code=302)
+
+@app.route("/authorize")
+def authorize():
+    response = "hello"
+    return response
 
 if __name__ == '__main__':
     app.run(port=3000)
