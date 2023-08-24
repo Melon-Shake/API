bcbfcce923bcfddf9dd2fa4c63f92df48ab52363
iamyanghee <yanghee.data@gmail.com>
2023-08-24 19:43:22 +0900

    spotify 권한 동의 페이지까지 확인 완료

--- load_spotify.py
+++ load_spotify.py
@@ -1,14 +1,22 @@
 from flask import Flask, redirect, url_for
+import client
+
+client_id = client.ID
+redirect_uri = client.REDIRECT_URI
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return redirect(url_for("authorize"),code=302)
 
-@app.route("/authorize")
+@app.route('/authorize')
 def authorize():
-    response = "hello"
+    response = redirect('https://accounts.spotify.com/authorize?'
+                        +f'client_id={client_id}'
+                        +f'&redirect_uri={redirect_uri}'
+                        +f'&response_type=code'
+                        )
     return response
 
 if __name__ == '__main__':
