b0c7fde2c8e3a4d7142c07543eb7c6b56a5f3b9b
Ho Yeon Jeong <2580jhy@naver.com>
2023-08-29 10:09:30 +0900

    fastapi 구현

--- lyric_fast_api.py
+++ lyric_fast_api.py
@@ -1,71 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-import requests
-from bs4 import BeautifulSoup
 from lyric import *
 app = FastAPI()
 
+
 GENIUS_API_KEY = "hvNyikfbrRz7IrjRN2wyrFwCc2YstwyCSsxcUAiwg9hbat_vNaEk8nqMBguxrlNt"
 
-class TrackInfo(BaseModel):
-    artist: str
-    track: str
+class input_data(BaseModel):
+    artist : str
+    track : str
+    track_id : int
+    GENIUS_API_KEY : str
 
-@app.post("/get_lyrics")
-async def get_lyrics(track_info: TrackInfo):
-    artist = track_info.artist
-    track = track_info.track
-
-    lyrics = lyric_search(artist, track, GENIUS_API_KEY)
-    return {"lyrics": lyrics}
-
-def lyric_search(artist, track, GENIUS_API_KEY):
-    lyric = musix_match_lyric_search(artist, track)
-    if lyric:
-        return lyric
+@app.post("/lyric_input/")
+def lyric_input(item : input_data):
+    artist = item.artist
+    track = item.track
+    track_id = item.track_id
+    GENIUS_API_KEY = item.GENIUS_API_KEY
+    result = lyric_search_and_input(artist,track,track_id,GENIUS_API_KEY)
+    if result:
+        return {"result" : f"Lyrics have been added to track_id : {track_id}"}
     else:
-        lyric = genius_unique_search(artist, track, GENIUS_API_KEY)[-1]
-        return lyric
-
-def musix_match_lyric_search(artist, track):
-    url = 'https://www.musixmatch.com/lyrics/'
-    headers = {'User-agent': 'Googlebot'}
-    result_url = url + artist + '/' + track
-
-    response = requests.get(result_url, headers=headers)
-
-    if response.status_code == 200:
-        page_content = response.content
-        soup = BeautifulSoup(page_content, 'html.parser')
-
-        lyrics_container = soup.find_all('span', class_='lyrics__content__ok')
-
-        lyrics_list = []
-
-        for container in lyrics_container:
-            lyrics_list.extend(container.stripped_strings)
-
-        lyrics = '\n'.join(lyrics_list)
-        return lyrics
-
-@app.post("/insert_lyrics")
-async def insert_lyrics(track_info: TrackInfo):
-    artist = track_info.artist
-    track = track_info.track
-
-    lyrics = lyric_search(artist, track, GENIUS_API_KEY)
-    if lyrics:
-        insert_data(lyrics, artist, track)
-        return {"message": "Lyrics inserted successfully"}
-    else:
-        return {"message": "Lyrics not found"}
-
-def insert_data(content, artist, track):
-    try:
-        # Connect to the database and insert data
-        # Your database connection and insertion code here
-        pass
-    except Exception as error:
-        return {"error": str(error)}
-
-# Other functions and definitions here (same as provided in your original code)
+        raise HTTPException(status_code=404, detail="Lyric ERR.")
