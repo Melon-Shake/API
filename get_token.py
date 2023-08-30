a7c15433cb6c07478fdcf905c893441b9a3adbef
iamyanghee <yanghee.data@gmail.com>
2023-08-30 12:35:32 +0900

    pipenv install fastapi --dev

--- get_token.py
+++ get_token.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, request
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import RedirectResponse
 import requests
 import base64
 import config.client as client
@@ -7,58 +8,63 @@ client_id = client.ID
 client_secrets = client.SECRETS
 redirect_uri = client.REDIRECT_URI
 
+app = FastAPI()
+
 def func_base64(input):
     input_byte = input.encode()
     output_byte = base64.b64encode(input_byte)
     output = output_byte.decode()
     return output
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.get("/")
 def index():
-    return redirect(url_for("authorize"))
+    return RedirectResponse(url="/authorize")
 
-@app.route('/authorize')
+@app.get("/authorize")
 def authorize():
-    return redirect('https://accounts.spotify.com/authorize?'
-                        +f'client_id={client_id}'
-                        +f'&redirect_uri={redirect_uri}'
-                        +f'&response_type=code'
-                        )
+    return RedirectResponse(
+        url=f'https://accounts.spotify.com/authorize?'
+        +f'client_id={client_id}'
+        +f'&redirect_uri={redirect_uri}'
+        +f'&response_type=code'
+    )
 
-@app.route('/callback')
-def callback():
-    code = request.args.get('code', None)
-    response = requests.post('https://accounts.spotify.com/api/token'
-                                ,data={
-                                    'grant_type': 'authorization_code'
-                                    ,'code': code
-                                    ,'redirect_uri': redirect_uri
-                                }
-                                ,headers={
-                                    'Authorization': 'Basic ' + func_base64(client_id + ':' + client_secrets)
-                                }
-                                )
-    
-    if response.status_code == 200 :
+@app.get("/callback")
+def callback(code: str):
+    response = requests.post('https://accounts.spotify.com/api/token',
+                             data={
+                                 'grant_type': 'authorization_code',
+                                 'code': code,
+                                 'redirect_uri': redirect_uri
+                             },
+                             headers={
+                                 'Authorization': 'Basic ' + func_base64(client_id + ':' + client_secrets)
+                             }
+                             )
+
+    if response.status_code == 200:
         response_json = response.json()
         refresh_token = response_json.get('refresh_token', None)
         access_token = response_json.get('access_token', None)
-        return refresh_token
-    else :
-        return response.status_code
+        return {"access_token": access_token}
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to get access token")
 
-def get_token_by_refresh_token(refresh_token):
-    requests.post('https://accounts.spotify.com/api/token'
-                    ,data={
-                        'grant_type': 'refresh_token'
-                        ,'refresh_token': refresh_token
-                    }
-                    ,headers={
-                        'Authorization': 'Basic ' + func_base64(client_id + ':' + client_secrets)
-                    }
-                    )
+@app.get("/refresh_token")
+def refresh_token(refresh_token: str):
+    response = requests.post('https://accounts.spotify.com/api/token',
+                             data={
+                                 'grant_type': 'refresh_token',
+                                 'refresh_token': refresh_token
+                             },
+                             headers={
+                                 'Authorization': 'Basic ' + func_base64(client_id + ':' + client_secrets)
+                             }
+                             )
 
-if __name__ == '__main__':
-    app.run(port=3000)
+    if response.status_code == 200:
+        response_json = response.json()
+        access_token = response_json.get('access_token', None)
+        return {"access_token": access_token}
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to refresh token")
