ca5abc31e3e938c00caf1af50cf50f5cfae25316
iamyanghee <yanghee.data@gmail.com>
2023-08-31 20:36:22 +0900

    my_api 엔드포인트 /token 추가

--- my_api.py
+++ my_api.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from model.spotify_client import SpotifyClientEntity
 
+
 api = FastAPI()
 
 @api.get('/')
@@ -16,6 +17,12 @@ def registrate(client:SpotifyClientEntity):
 @api.get('/registrate')
 def registrate(id:str,redirect_uri:str) :
     return id+redirect_uri
+
+from update_token import return_token
+@api.get('/token')
+def update_token():
+    access_token = return_token()
+    return access_token
 
 if __name__ == "__main__":
     uvicorn.run(api, host="127.0.0.1", port=8000)
