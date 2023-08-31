414ddd23a8384ce97087e167293be480cb7b89a6
iamyanghee <yanghee.data@gmail.com>
2023-08-31 20:20:20 +0900

    일단 access_token 발급까지 완료

--- update_token.py
+++ update_token.py
@@ -1,11 +1,31 @@
 from model.spotify_token import SpotifyTokenORM, SpotifyTokenEntity
+from model.spotify_client import SpotifyClientORM, SpotifyClientEntity
 from model.database import session_scope
 
+import requests, json
+
+def func_base64(input):
+    import base64
+    input_byte = input.encode()
+    output_byte = base64.b64encode(input_byte)
+    output = output_byte.decode()
+    return output
+
 if __name__ == '__main__' :
-    orm = SpotifyTokenORM({'id':1,'value':'aaaa','is_expired':False})
-    entity = SpotifyTokenEntity.model_validate(orm)
     
-    print(type(orm))
-    print(orm)
-    print(type(entity))
-    print(entity)
+    with session_scope() as session :
+        client = session.query(SpotifyClientORM).one()
+
+        response = requests.post('https://accounts.spotify.com/api/token'
+                      ,headers={
+                          'Authorization': 'Basic '+func_base64(f'{client.id}:{client.secret}')
+                      }
+                      ,data={
+                          'grant_type': 'refresh_token'
+                          ,'refresh_token': client.refresh_token
+                      }
+                      )
+    if response.status_code == 200 :
+        result = response.json()
+        access_token = result.get('access_token')
+        print(access_token)
