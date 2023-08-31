54884f85537c4cdd0579cba7bed2846813fcb178
iamyanghee <yanghee.data@gmail.com>
2023-08-31 16:46:06 +0900

    spotify_client 등록하는 과정까지만 코드와 파일 정리

--- model/spotify_client.py
+++ model/spotify_client.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String
 from sqlalchemy.sql.schema import Column
 
-from config.database import Base, session_scope
+from config.database import Base
 from pydantic import BaseModel, ConfigDict
 
 class SpotifyClientORM(Base) :
@@ -12,23 +12,11 @@ class SpotifyClientORM(Base) :
     secret = Column(String, nullable=False)
     redirect_uri = Column(String, nullable=False)
 
-    def __init__(self, user=None, id=None, secret=None, redirect_uri=None):
-        self.user = user
-        self.id = id
-        self.secret = secret
-        self.redirect_uri = redirect_uri
-
-    def set(self, client):
-        with session_scope() as session :
-            session.add(client)
-            session.commit()
-
-    def get(self, client_user):
-        with session_scope() as session :
-            clients = session.query(SpotifyClientORM).all()
-            for client in clients :
-                if client.user == client_user :
-                    return SpotifyClientORM(client.user,client.id,client.secret,client.redirect_uri)
+    def __init__(self, client) :
+        self.user = client.get('user')
+        self.id = client.get('id')
+        self.secret = client.get('secret')
+        self.redirect_uri = client.get('redirect_uri')
 
 class SpotifyClientEntity(BaseModel) :
     model_config = ConfigDict(from_attributes=True)
---
+++ registrate.py
@@ -0,0 +1,58 @@
+from model.spotify_client import SpotifyClientORM, SpotifyClientEntity
+from config.database import session_scope
+
+import requests
+import json
+
+if __name__ == '__main__':
+    
+    client = {
+        'user': 'iamsophie'
+        , 'id': '42bf3e0e18094c5a8bdd940eb278ca5d'
+        , 'secret': '696d03cb2ad84ac693342793df26bc09'
+        , 'redirect_uri': 'http://localhost:8000/callback'
+     }
+    
+    client_orm = SpotifyClientORM(client)
+
+    with session_scope() as session :
+        # session.add(client_orm)
+        
+        client_orm = session.query(SpotifyClientORM).filter_by(user=client.get('user')).first()
+        client_entity = SpotifyClientEntity.model_validate(client_orm)
+        client_json = SpotifyClientEntity.model_dump_json(client_entity)
+
+        response_post = requests.post('http://localhost:8000/registrate'
+                                 ,headers={'Content-Type': 'application/json'}
+                                 ,data=client_json
+                                 )
+        if response_post.status_code == 200 :
+            result_post = response_post.json()
+        else :
+            print(response_post.status_code)
+        
+        response_get = requests.get('http://localhost:8000/registrate?'
+                                    +f'id={client.get("id")}'
+                                    +f'&redirect_uri={client.get("redirect_uri")}'
+                                    )
+        if response_get.status_code == 200 :
+            result_get = response_get.json()
+        else :
+            print(response_get.status_code)
+
+    def checks() :
+        print(type(client_orm))
+        print(client_orm)
+        
+        print(type(client_entity))
+        print(client_entity)
+
+        print(type(client_json))
+        print(client_json)
+
+        print(type(result_post))
+        print(result_post)
+
+        print(type(result_get))
+        print(result_get)
+    checks()
