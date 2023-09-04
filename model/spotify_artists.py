4360b51ad3e9dcad5f7f38dbb77359cfa3ef370c
iamyanghee <yanghee.data@gmail.com>
2023-09-04 19:33:51 +0900

    spotify artists 테이블 새로 생성 및 해당 구조로 모델링 완료

---
+++ model/spotify_artists.py
@@ -0,0 +1,34 @@
+from database import Base
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy.sql.schema import Column
+from sqlalchemy import String
+
+class SpotifyArtistsORM(Base) :
+    __tablename__ = 'spotify_artists'
+
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=True)
+    uri = Column(String, nullable=True)
+    href = Column(String, nullable=True)
+    external_urls = Column(String, nullable=True)
+    images_url = Column(String, nullable=True)
+    genres = Column(String, nullable=True)
+    popularity = Column(String, nullable=True)
+
+    def __init__(self, artists) :
+        pass
+
+class SpotifyArtistsEntity(BaseModel) :
+    model_config = ConfigDict(from_attributes=True)
+
+    id: str
+    name: str
+    images_url: str
+
+if __name__ == '__main__' :
+    from sqlalchemy import inspect
+    mapper = inspect(SpotifyArtistsORM)
+    table_name = mapper.persist_selectable.name
+    column_names = [column.key for column in mapper.columns]
+    print("Table Name:", table_name)
+    print("Column Names:", column_names)
