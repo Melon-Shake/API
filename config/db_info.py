96e4149ba8d8fafdfea9d9794d7c21c848239075
mews2000 <mews2000@gmail.com>
2023-09-04 12:02:36 +0900

    중요 파일 삭제

--- config/db_info.py
+++
@@ -1,7 +0,0 @@
-db_params = {
-    "database" : "postgres",
-    "user" : "postgres",
-    "password" : "12345678",
-    "host" : "database-1.coea55uwjt5p.ap-northeast-1.rds.amazonaws.com",
-    "port" : "5432"
-}
