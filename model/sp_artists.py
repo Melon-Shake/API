3e52a429a4c2d1512ba1c7c08f7d808559298d93
iamyanghee <yanghee.data@gmail.com>
2023-09-05 10:50:13 +0900

    기존 sp_ 모델 drop

--- model/sp_artists.py
+++
@@ -1,9 +0,0 @@
-from model.database import Base
-from pydantic import BaseModel, ConfigDict
-from sqlalchemy.sql.schema import Column
-
-class SpotifyArtistsORM(Base) :
-    pass
-
-class SpotifyArtistsEntity(BaseModel) :
-    pass
--- model/sp_tracks.py
+++
@@ -1,9 +0,0 @@
-from model.database import Base
-from pydantic import BaseModel, ConfigDict
-from sqlalchemy.sql.schema import Column
-
-class SpotifyTracksORM(Base) :
-    pass
-
-class SpotifyTracksEntity(BaseModel) :
-    pass
