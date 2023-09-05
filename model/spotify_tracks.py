f84dfb9826b49c81370a6c3f0919b95d6d4c829f
iamyanghee <yanghee.data@gmail.com>
2023-09-05 10:49:43 +0900

    spotify tracks 테이블 새로 생성 및 해당 구조로 모델링 완료

---
+++ model/spotify_tracks.py
@@ -0,0 +1,37 @@
+from database import Base
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy.sql.schema import Column
+from sqlalchemy import String
+
+class SpotifyTracksORM(Base) :
+    __tablename__ = 'sp_tracks'
+
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=True)
+    uri = Column(String, nullable=True)
+    href = Column(String, nullable=True)
+    external_urls = Column(String, nullable=True)
+    duration_ms = Column(String, nullable=True)
+    explicit = Column(String, nullable=True)
+    restrictions_reason = Column(String, nullable=True)
+    is_playable = Column(String, nullable=True)
+    linked_form = Column(String, nullable=True)
+    available_markets = Column(String, nullable=True)
+    disc_number = Column(String, nullable=True)
+    track_number = Column(String, nullable=True)
+    popularity = Column(String, nullable=True)
+    preview_url = Column(String, nullable=True)
+    is_local = Column(String, nullable=True)
+    albums_id = Column(String, nullable=True)
+    artists_ids = Column(String, nullable=True)
+
+class SpotifyTracksEntity(BaseModel) :
+    model_config = ConfigDict(from_attributes=True)
+
+if __name__ == '__main__' :
+    from sqlalchemy import inspect
+    mapper = inspect(SpotifyTracksORM)
+    table_name = mapper.persist_selectable.name
+    column_names = [column.key for column in mapper.columns]
+    print("Table Name:", table_name)
+    print("Column Names:", column_names)
