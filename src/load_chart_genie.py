5a1c48b9c557b756a905b396066bca54c8c901d5
iamyanghee <yanghee.data@gmail.com>
2023-09-06 11:18:25 +0900

    지니차트 API 호출 응답 확인

--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -1,9 +1,17 @@
 import requests
 
 if __name__ == '__main__' :
-    response = requests.get('')
+    response = requests.post('https://app.genie.co.kr/chart/j_RealTimeRankSongList.json'
+                             ,headers={
+                                 'Content_Type' : 'application/x-www-form-urlencoded'
+                             }
+                             ,data={
+                                 'dict': 'R'
+                                 ,'pg_size': '200'
+                             }
+                            )
 
     if response.status_code == 200 :
         pass
-    
+
     else : print(response.status_code)
