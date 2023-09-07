4e0f0a2b9f93374dfc17092345e09b6b334f328a
iamyanghee <yanghee.data@gmail.com>
2023-09-07 10:56:00 +0900

    orm 모델 완료

--- model/chart_genie.py
+++ model/chart_genie.py
@@ -8,7 +8,26 @@ from sqlalchemy import String, Integer, ARRAY
 class ChartGenieORM(Base) :
     __tablename__ = 'genie_chart'
 
-    id = Column(String, primary_key=True)
+    song_id = Column(String, primary_key=True)
+    song_name = Column(String, nullable=True)
+    artist_id = Column(String, nullable=True)
+    artist_name = Column(String, nullable=True)
+    album_id = Column(String, nullable=True)
+    album_name = Column(String, nullable=True)
+    album_img_path = Column(String, nullable=True)
+    rank_no = Column(String, nullable=True)
+    pre_rank_no = Column(String, nullable=True)
+
+    def __init__(self, genie) :
+        self.song_id = genie.SONG_ID
+        self.song_name = genie.SONG_NAME
+        self.artist_id = genie.ARTIST_ID
+        self.artist_name = genie.ARTIST_NAME
+        self.album_id = genie.ALBUM_ID
+        self.album_name = genie.ALBUM_NAME
+        self.album_img_path = genie.ALBUM_IMG_PATH
+        self.rank_no = genie.RANK_NO
+        self.pre_rank_no = genie.PRE_RANK_NO
 
 class ChartGenie(BaseModel) :
     SONG_ID: str
--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -5,7 +5,7 @@ import os
 root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
 sys.path.append(root_path)
 
-from model.chart_genie import ChartGenie
+from model.chart_genie import ChartGenie, ChartGenieORM
 
 if __name__ == '__main__' :
     response = requests.post('https://app.genie.co.kr/chart/j_RealTimeRankSongList.json'
@@ -21,7 +21,8 @@ if __name__ == '__main__' :
     if response.status_code == 200 :
         responsed_data = response.json().get('DataSet').get('DATA')
 
-        for x in responsed_data :
-            entity = ChartGenie(**x)
+        for e in responsed_data :
+            entity = ChartGenie(**e)
+            orm = ChartGenieORM(entity)
 
     else : print(response.status_code)
