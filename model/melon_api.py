7146a22be94820f34f8e76812e7448e8ae8aaf6c
bksooon <sbka0704@nate.com>
2023-09-07 14:38:46 +0900

    v0.2.0/bksonchart_melon made

--- model/melon_api.py
+++ model/melon_api.py
@@ -2,14 +2,23 @@ from pydantic import BaseModel, ConfigDict
 from typing import Dict, List, Union
 from typing import List, Optional
 
-# from model.
+class Artistinfo(BaseModel):
+  ARTISTID: str
+  ARTISTNAME: str
+# 
+class GenreInfo(BaseModel):
+  GENRECODE: Union[str, int]
+  GENRENAME: Union[str, int]
+  
 
 class ChartMelon(BaseModel):
   SONGID: int
   SONGNAME: str
   ALBUMID: int
   ALBUMNAME: str
-  ARTISTID: List[Dict[str,Union[str,int]]]
-  ARTISTNAME: List[Dict[str,Union[str,None]]]
-  GENRECODE: List[Dict[str, Union[str, None]]]
-  # GENRENAME: List[Dict[str, Union[str, None]]]
+  ARTISTLIST : List[Artistinfo]
+  GENRELIST: List[GenreInfo]
+  CURRANK: int
+  PASTRANK: str
+  ALBUMIMG: str
+  ISSUEDATE: str
