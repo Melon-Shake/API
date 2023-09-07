26dd86bf4a57d5449e0d3c7e7d250cb2dad3740e
bksooon <sbka0704@nate.com>
2023-09-07 09:58:27 +0900

    v0.2.0/bkson/chart_melon make loadchartmelon

--- model/melon_api.py
+++ model/melon_api.py
@@ -1,2 +1,12 @@
-from pydantic import BaseModel
-from typing 
+from pydantic import BaseModel, ConfigDict
+from typing import Dict, List, Union
+
+# from model.
+
+class ChartMelon(BaseModel):
+  SONGID: int
+  # SONGNAME: str
+  # ALBUMID: str
+  # ALBUMNAME: str
+  # ARTISTID: List[Dict[str,Union(str,None)]]
+  # ARTISTNAME: List[Dict[str,Union(str,None)]]
