88aff1df756c43a1e804786f51f9d74d55c061fe
iamyanghee <yanghee.data@gmail.com>
2023-09-07 12:10:30 +0900

    테이블 적재 완료

--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -6,6 +6,7 @@ root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
 sys.path.append(root_path)
 
 from model.chart_genie import ChartGenie, ChartGenieORM
+from model.database import session_scope
 
 if __name__ == '__main__' :
     response = requests.post('https://app.genie.co.kr/chart/j_RealTimeRankSongList.json'
@@ -24,5 +25,8 @@ if __name__ == '__main__' :
         for e in responsed_data :
             entity = ChartGenie(**e)
             orm = ChartGenieORM(entity)
+
+            with session_scope() as session :
+                session.add(orm)
 
     else : print(response.status_code)
