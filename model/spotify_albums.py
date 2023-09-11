e3a0126057d35c7e850b4556b504825cf704f4ad
iamyanghee <yanghee.data@gmail.com>
2023-09-11 16:00:57 +0900

    spotify albums 모델 수정 완료

--- model/spotify_albums.py
+++ model/spotify_albums.py
@@ -5,18 +5,51 @@ sys.path.append(root_path)
 
 from model.database import Base
 from pydantic import BaseModel, ConfigDict
+from typing import Dict, List, Union, Optional
 from sqlalchemy.sql.schema import Column
-from sqlalchemy import String
+from sqlalchemy import String, Integer, ARRAY
+
+class Images(BaseModel) :
+    url: str
+class ExternalUrls(BaseModel):
+    spotify: str
 
 class SpotifyAlbums(BaseModel) :
     model_config = ConfigDict(from_attributes=True)
 
-    pass
+    id: str
+    name: str
+    uri: str
+    href: str
+    external_urls: ExternalUrls
+    images: List[Images]
+    album_type: str
+    total_tracks: int
+    release_date: str
+    release_date_precision: str
 
 class SpotifyAlbumsORM(Base) :
     __tablename__ = 'spotify_albums'
 
     id = Column(String, primary_key=True)
+    name = Column(String, nullable=True)
+    uri = Column(String, nullable=True)
+    href = Column(String, nullable=True)
+    external_urls = Column(String, nullable=True)
+    images_url = Column(String, nullable=True)
+    album_type = Column(String, nullable=True)
+    total_tracks = Column(Integer, nullable=True)
+    release_date = Column(String, nullable=True)
+    release_date_precision = Column(String, nullable=True)
 
-    def __init__(self, albums) :
-        pass
+    def __init__(self, albums:SpotifyAlbums) :
+        self.id = albums.id
+        self.name = albums.name
+        self.uri = albums.uri
+        self.href = albums.href
+        self.external_urls = albums.external_urls.spotify
+        self.images_url = albums.images[0].url
+        self.album_type = albums.album_type
+        self.total_tracks = albums.total_tracks
+        self.release_date = albums.release_date
+        self.release_date_precision = albums.release_date_precision
--- src/search_spotify.py
+++ src/search_spotify.py
@@ -5,6 +5,7 @@ sys.path.append(root_path)
 
 from model.database import session_scope
 from model.spotify_artists import SpotifyArtists, SpotifyArtistsORM
+from model.spotify_albums import SpotifyAlbums, SpotifyAlbumsORM
 from model.spotify_search import SpotifySearch, SpotifySearchArtists
 
 from src.get_token import update_token, return_token
@@ -12,7 +13,7 @@ from src.get_token import update_token, return_token
 import requests
 
 if __name__ == '__main__':
-    update_token('iamsophie')
+    # update_token('iamsophie')
     access_token = return_token()
     response = requests.get('https://api.spotify.com/v1/search?'
                             +'q=아이유'
@@ -24,14 +25,18 @@ if __name__ == '__main__':
     
     if response.status_code == 200 :
         responsed_data = response.json()
+        # print(responsed_data.get('albums').get('items')[0])
+
         parsed_data = SpotifySearch(**responsed_data)
 
+        artists = parsed_data.artists.items
+        for entity in artists :
+            orm = SpotifyArtistsORM(entity)
+            with session_scope() as session :
+                session.add(orm)
+
         albums = parsed_data.albums.items
-        print(type(albums))
-        # print(albums[0])
-
-        # for entity in artists :
-        #     orm = SpotifyArtistsORM(entity)
-
-        #     with session_scope() as session :
-        #         session.add(orm)
+        for entity in albums :
+            orm = SpotifyAlbumsORM(entity)
+            with session_scope() as session :
+                session.add(orm)
