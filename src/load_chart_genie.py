cf0fd93d1065e9cb012c530041eed78f3efb654f
jungssg <younwjdtjr@naver.com>
2023-09-11 17:39:48 +0900

    unquote 사용 encoding error 해결

--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -29,68 +29,60 @@ if __name__ == '__main__' :
         album_img = []
 
         entries = {}
-        for itme in responsed_data:
-            for index, item in enumerate(responsed_data):
-                # 제목 디코딩
-                pre_track_title = item['SONG_NAME']
-                track_title = urllib.parse.unquote(pre_track_title)
+        for index, item in enumerate(responsed_data):
+            # 제목 디코딩
+            pre_track_title = item['SONG_NAME']
+            track_title = urllib.parse.unquote(pre_track_title)
+            
+            # 아티스트 디코딩
+            pre_artists = item.get('ARTIST_NAME')
+            artists = urllib.parse.quote(pre_artists)  # URL 디코딩
+
+            # 앨범제목
+            pre_album = item['ALBUM_NAME']
+            album = urllib.parse.unquote(pre_album)
+            entries[index] = [track_title, artists, album]
                 
-                # 아티스트 디코딩
-                pre_artists = item.get('ARTIST_NAME')
-                artists = urllib.parse.unquote(pre_artists)  # URL 디코딩
-
-                # 앨범제목
-                pre_album = item['ALBUM_NAME']
-                album = urllib.parse.unquote(pre_album)
-                entries[index+1] = [track_title, artists, album]
-        print(entries)
+        for i in range(len(entries)):
+            
+            q = entries[i][0] + " " + entries[i][1]+ " " + entries[i][2]
+            
+            url = f'https://api.spotify.com/v1/search?q={q}&type=track&limit=1'
+            headers = {
+                'Authorization': 'Bearer '+access_token
+            }
+            response_sp = requests.get(url, headers=headers)
+            if response_sp.status_code == 200:
+                sp_json = response_sp.json()
+                artists_sp = []
+                song_name.append(sp_json['tracks']['items'][0]['name'])
+                album_name.append(sp_json['tracks']['items'][0]['album']['name'])
+                album_img.append(sp_json['tracks']['items'][0]['album']['images'][0]['url'])
                 
-        # for i in range(len(responsed_data)):
+                for j in range(len(sp_json['tracks']['items'][0]['artists'])):
+                    artists_sp.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
+                artist_name.append(artists_sp)
+            else:
+                print(f'{i} : {response_sp.status_code}')
 
-        #     q = responsed_data[i]['SONG_NAME'] +' '+ responsed_data[i]['ARTIST_NAME'] + ' ' + responsed_data[i]['ALBUM_NAME']
-        #     url = f'https://api.spotify.com/v1/search?q={q}&type=track&limit=1'
-        #     headers = {
-        #         'Authorization': 'Bearer '+access_token
-        #     }
-        #     response_sp = requests.get(url, headers=headers)
-        #     if response_sp.status_code == 200:
-        #         sp_json = response_sp.json()
-        #         return_data ={}
-        #         artists = []
-        #         song_name.append(sp_json['tracks']['items'][0]['name'])
-        #         album_name.append(sp_json['tracks']['items'][0]['album']['name'])
-        #         album_img.append(sp_json['tracks']['items'][0]['album']['images'][0]['url'])
-                
-        #         for j in range(len(sp_json['tracks']['items'][0]['artists'])):
-        #             artists.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
-        #         artist_name.append(artists)
-        #     else:
-        #         print(f'{i} : {response_sp.status_code}')
+        print(len(responsed_data))
+        print(len(song_name))
 
-        # print(len(responsed_data))
-        # print(len(song_name))
-        # print(song_name[0])
-        # print(song_name[98])
-        
-        
-        # print(artist_name)
-        # print(album_name)
-        # print(len(responsed_data))
-
-        
         # print(f"곡 : {len(song_name)}   가수 : {len(artist_name)}    앨범 : {len(album_name)}")
-        # for i in range(len(responsed_data)):
-        #     responsed_data[i]['SONG_NAME'] = song_name[i]
-        #     responsed_data[i]['ARTIST_NAME'] = artist_name[i]
-        #     responsed_data[i]['ALBUM_NAME'] = album_name[i]
-        #     responsed_data[i]['ALBUM_IMG_PATH'] = album_img[i]
+        for i in range(len(responsed_data)):
+            responsed_data[i]['SONG_NAME'] = song_name[i]
+            responsed_data[i]['ARTIST_NAME'] = artist_name[i]
+            responsed_data[i]['ALBUM_NAME'] = album_name[i]
+            responsed_data[i]['ALBUM_IMG_PATH'] = album_img[i]
         
         # print(responsed_data)
-        # for e in responsed_data :
-            # entity = ChartGenie(**e)
-            # orm = ChartGenieORM(entity)
-# 
-            # with session_scope() as session :
-                # session.add(orm)
-# 
-    # else : print(response.status_code)
+        
+        # print(responsed_data)
+        for e in responsed_data :
+            entity = ChartGenie(**e)
+            orm = ChartGenieORM(entity)
+
+            with session_scope() as session :
+                session.add(orm)
+
+    else : print(response.status_code)
