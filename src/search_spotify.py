7262871a578f10963c754cc0dff93f0734056c9f
iamyanghee <yanghee.data@gmail.com>
2023-09-12 01:49:21 +0900

    search_spotify 함수와 load_spotify 함수로 기능 분리

--- src/search_spotify.py
+++ src/search_spotify.py
@@ -10,11 +10,10 @@ from src.get_token import update_token, return_token
 
 import requests
 
-if __name__ == '__main__':
-    # update_token('iamsophie')
+def search_spotify(input:str) :
     access_token = return_token()
     response = requests.get('https://api.spotify.com/v1/search?'
-                            +'q=아이유'
+                            +'q={keyword}'.format(keyword=input)
                             +'&type=artist%2Calbum%2Ctrack'
                       ,headers={
                           'Authorization': 'Bearer '+ access_token
@@ -23,14 +22,22 @@ if __name__ == '__main__':
     
     if response.status_code == 200 :
         responsed_data = response.json()
-
         parsed_data = Spotify.Search(**responsed_data)
+        return parsed_data
 
-        with session_scope() as session :
-            for entity in parsed_data.tracks.items :
-                tracks = Spotify.TracksORM(entity)
-                albums = Spotify.AlbumsORM(entity.album)
-                artists = [Spotify.ArtistsORM(e) for e in entity.artists]
-                session.add(tracks)
-                session.add(albums)
-                session.add_all(artists)
+def load_spotify(data:Spotify.Search) :
+    with session_scope() as session :
+        for entity in data.tracks.items :
+            tracks = Spotify.TracksORM(entity)
+            albums = Spotify.AlbumsORM(entity.album)
+            artists = [Spotify.ArtistsORM(e) for e in entity.artists]
+            session.add(tracks)
+            session.add(albums)
+            session.add_all(artists)
+
+if __name__ == '__main__':
+    # update_token('iamsophie')
+    access_token = return_token()
+
+    search_result = search_spotify('아이유')
+    load_spotify(search_result)
