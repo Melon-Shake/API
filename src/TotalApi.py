5f15d8de9be7c84a73cf9998effa44f36cc749f6
json9101 <sjunsung981@gmail.com>
2023-09-11 16:24:43 +0900

    if exists command 삭제

--- src/TotalApi.py
+++ src/TotalApi.py
@@ -304,14 +304,13 @@ def get_daily_search_ranking():
     for _, (keyword, search_rank) in enumerate(search_ranking):
         cursor.execute(value_check_query, (keyword,))
         if cursor.fetchone():
-            if exists:
-                if search_rank != prev_search_rank:  # 동일한 순위가 아니면 순위 업데이트
-                    rank += 1
-            result[rank] = keyword
-            prev_search_rank = search_rank
+            if search_rank != prev_search_rank:  # 동일한 순위가 아니면 순위 업데이트
+                rank += 1
+        result[rank] = keyword
+        prev_search_rank = search_rank
             
-            if rank >= 20:  # 20위까지만 결과 저장
-                break
+        if rank >= 20:  # 20위까지만 결과 저장
+            break
     
     connection.close()
     return result
