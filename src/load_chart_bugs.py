54fdcc10fe2ef51ed41ba2dc23b06df2b89e3d4c
bksooon <sbka0704@nate.com>
2023-09-11 13:48:36 +0900

    230911/bkson/bugs  cd /home/bk_son/code/melon-shake/api ; /usr/bin/env /home/bk_son/.pyenv/versions/3.9.16/bin/python /home/bk_son/.vscode-server/extensions/ms-python.python-2023.16.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 43629 -- /home/bk_son/code/melon-shake/api/src/load_chart_bugs.py

--- src/load_chart_bugs.py
+++ src/load_chart_bugs.py
@@ -25,10 +25,19 @@ if __name__ == '__main__':
     response = requests.post(_CHART_API_URL, headers=headers, data=data)
     if response.status_code == 200 :
         responsed_data = response.json().get('list')
-    
-        for x in responsed_data :
-            entity = BugsEntity(**x)
-            orm = BugsORM(entity)
+        entries = {}
+        for itme in responsed_data:
+            for index, item in enumerate(responsed_data):
+                track_title = item['track_title']
+                artists = item.get('artists')
+                artist_pre = []
+                for artist in artists:
+                    artist_nm = artist['artist_nm']
+                    artist_pre.append(artist_nm)
+                entries[index+1] = [track_title, artist_pre]
+        # for x in responsed_data :
+        #     entity = BugsEntity(**x)
+        #     orm = BugsORM(entity)
 
-            with session_scope() as session :
-                session.add(orm)
+        #     with session_scope() as session :
+        #         session.add(orm)
