d8c01c0c06e1116abe5115c6d9e1cd259daac2d4
iamyanghee <yanghee.data@gmail.com>
2023-09-12 01:21:02 +0900

    엮어서 저장한 것들의 값 검증 확인 완료

--- src/search_spotify.py
+++ src/search_spotify.py
@@ -26,11 +26,25 @@ if __name__ == '__main__':
 
         parsed_data = Spotify.Search(**responsed_data)
 
+        # with session_scope() as session :
+        #     for entity in parsed_data.tracks.items :
+        #         tracks = Spotify.TracksORM(entity)
+        #         albums = Spotify.AlbumsORM(entity.album)
+        #         artists = [Spotify.ArtistsORM(e) for e in entity.artists]
+        #         session.add(tracks)
+        #         session.add(albums)
+        #         session.add_all(artists)
+        
+        # with session_scope() as session :
+        #     rows = session.query(Spotify.TracksORM.artists_ids).distinct().all()
+        #     ids_list = [id for row in rows for id in row]
+        #     ids_unique = list(set(id for ids in ids_list for id in ids))
+            
+        #     ids_artists = session.query(Spotify.ArtistsORM.id).distinct().all()
+        #     print(all(id_artists.id in ids_unique for id_artists in ids_artists))
+
         with session_scope() as session :
-            for entity in parsed_data.tracks.items :
-                tracks = Spotify.TracksORM(entity)
-                albums = Spotify.AlbumsORM(entity.album)
-                artists = [Spotify.ArtistsORM(e) for e in entity.artists]
-                session.add(tracks)
-                session.add(albums)
-                session.add_all(artists)
+            rows = session.query(Spotify.TracksORM.album_id).distinct().all()
+            ids_unique = list(row.album_id for row in rows)
+            ids_albums = session.query(Spotify.AlbumsORM.id).distinct().all()
+            print(all(id_albums.id in ids_unique for id_albums in ids_albums))
