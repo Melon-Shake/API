a15ab5a14258a77889c744bbde3d3a82e17c1589
bksooon <sbka0704@nate.com>
2023-09-12 15:47:28 +0900

    230912/bkson/integrated_chart init

--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -42,28 +42,31 @@ if __name__ == '__main__' :
 
         entries = {}
         for index, item in enumerate(responsed_data):
+            
             # 제목 디코딩
             pre_track_title = item['SONG_NAME']
             track_title = urllib.parse.unquote(pre_track_title)
             cleaned_track = re.sub(r'\([^)]*\)', '', track_title)
+            
             # 예외 처리
             if cleaned_track == '이브, 프시케 그리고 푸른 수염의 아내':
                 cleaned_track = 'Eve, Psyche & The Bluebeard’s wife'
            
             # 아티스트 디코딩
             pre_artists = item.get('ARTIST_NAME')
-            artists = urllib.parse.unquote(pre_artists)  # URL 디코딩
+            artists = urllib.parse.unquote(pre_artists) 
             cleaned_artist = re.sub(r'\([^)]*\)', '', artists)
+            
             #예외 처리
             if cleaned_artist == '#안녕':
                 cleaned_artist = urllib.parse.quote(pre_artists)
+                
             # 앨범제목
             pre_album = item['ALBUM_NAME']
             album = urllib.parse.unquote(pre_album)
             cleaned_album = re.sub(r'\([^)]*\)', '', album)
             
             entries[index] = [cleaned_track, cleaned_artist, cleaned_album]
-        # print(responsed_data)
 
     for i in range(len(entries)):
         
@@ -101,19 +104,10 @@ if __name__ == '__main__' :
                 for j in range(len(sp_json['tracks']['items'][0]['artists'])):
                     artists_sp.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
                 artist_name.append(', '.join(artists_sp))
-    # for i in range(len(responsed_data)):
         responsed_data[i]['SONG_NAME'] = song_name[i]
         responsed_data[i]['ARTIST_NAME'] = artist_name.pop()
         responsed_data[i]['ALBUM_NAME'] = album_name[i]
         responsed_data[i]['ALBUM_IMG_PATH'] = album_img[i]
-        # elseif response_sp.status_code == 400:
-        #     print(f'{i} : {response_sp.status_code}')
-
-    print(len(responsed_data))
-    print(len(song_name))
-
-        
-        # print(responsed_data)
 
     for e in responsed_data :
         entity = ChartGenie(**e)
@@ -123,14 +117,3 @@ if __name__ == '__main__' :
             session.add(orm)
 
     else : print(response.status_code)
-    
-    
-
-# 원래 문자열
-# original_string = "Smoke (Prod. by Dynamicduo & Padi) 심(心)"
-
-# # 정규 표현식을 사용하여 괄호와 괄호 안의 내용을 제거
-# cleaned_string = re.sub(r'\([^)]*\)', '', original_string)
-
-# # 결과 출력
-# print(cleaned_string)
