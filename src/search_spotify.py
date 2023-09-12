a5b13ce5a485f18db65bfd0b382af077917376a9
iamyanghee <yanghee.data@gmail.com>
2023-09-12 23:11:47 +0900

    search_spotify 반환받아 deduplicate 한 후 tracks 기준으로 albums, artists 까지 적재 완료

--- src/search_spotify.py
+++ src/search_spotify.py
@@ -26,38 +26,59 @@ def search_spotify(input:str) :
         responsed_data = response.json()
         parsed_data = Spotify.Search(**responsed_data)
         return parsed_data
+    
+def deduplicate(models) :
+    ids_uniq = set(model.id for model in models)
+    uniq = list()
+    for model in models :
+        if model.id in ids_uniq :
+            uniq.append(model)
+            ids_uniq.remove(model.id)
+    return uniq
 
 def load_spotify(data:Spotify.Search) :
-    unique_tracks = set(str(track) for track in data.tracks.items)
-    unique_albums = set(str(track.album) for track in data.tracks.items)
-    unique_artists = set(str(artist) for track in data.tracks.items for artist in track.artists)
+    tracks = data.tracks.items
+    albums = [track.album for track in tracks]
+    artists = [artist for track in tracks for artist in track.artists]
 
-    tracks = [Spotify.TracksORM(track) for track in unique_tracks]
-    albums = [Spotify.AlbumsORM(album) for album in unique_albums]
-    artists = [Spotify.ArtistsORM(artist) for artist in unique_artists]
+    tracks_orm = [Spotify.TracksORM(track) for track in deduplicate(tracks)]
+    albums_orm = [Spotify.AlbumsORM(album) for album in deduplicate(albums)]
+    artists_orm = [Spotify.ArtistsORM(artist) for artist in deduplicate(artists)]
 
     with session_scope() as session :
-        session.add_all(tracks)
-        session.add_all(albums)
-        session.add_all(artists)
+        session.add_all(tracks_orm)
+        session.add_all(albums_orm)
+        session.add_all(artists_orm)
+
+def convert_timestamp(timestamp:str) :
+    minutes = timestamp // 60
+    seconds = timestamp % 60
+    return f'{minutes}:{seconds}'
 
 def format_search(data:Spotify.Search) :
-    tracks = [Spotify.TracksORM(track) for track in data.tracks.items]
-    albums = [Spotify.AlbumsORM(track.album) for track in data.tracks.items]
-    artists = [Spotify.ArtistsORM(artist) for track in data.tracks.items for artist in track.artists]
-
     search_output = dict()
-    search_output['tracks'] = [{'name':track.name, 'img':None, 'artist':None, 'duration':None} for track in tracks]
-    search_output['albums'] = [{'name':album.name, 'img':album.images_url, 'artist':None, 'release_year':None} for album in albums]
-    search_output['artists'] = [{'name':artist.name, 'img':None} for artist in artists]
+    # search_output['tracks'] = [{'name':track.name, 'img':None, 'artist':None, 'duration':None} for track in tracks]
+    # search_output['albums'] = [{'name':album.name, 'img':album.images_url, 'artist':None, 'release_year':None} for album in albums]
+    # search_output['artists'] = [{'name':artist.name, 'img':None} for artist in artists]
+
+    # for track in data.tracks.items :
+        
+    #     track.name
+    #     track.album.images[0].url
+    #     ', '.join([artist.name for artist in track.artists])
+    #     convert_timestamp(track.duration_ms)
+
+    #     track.album.name
+    #     track.album.images[0].url
+    #     ', '.join([artist.name for artist in track.album.artists])
 
     return search_output
 
 if __name__ == '__main__':
-    # update_token('iamsophie')
+    update_token('iamsophie')
     access_token = return_token()
 
     search_result = search_spotify('아이유')
-    # load_spotify(search_result)
-    search_output = format_search(search_result)
-    print(search_output.get('tracks')[0])
+    load_spotify(search_result)
+    # search_output = format_search(search_result)
+    # print(search_output)
