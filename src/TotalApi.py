50171c308cd02f748d6192405ea6c179fb536723
json9101 <57518426+json9101@users.noreply.github.com>
2023-09-12 11:45:43 +0900

    V0.2.0/ranking boost 이상한 랭킹 나오는거 수정 (#108)
    * fix ranking error( rank 0부터 나오는거 수정 ,있는값만 나오게 변경)

--- src/TotalApi.py
+++ src/TotalApi.py
@@ -283,53 +283,39 @@ def get_daily_search_ranking():
     cursor = connection.cursor()
 
     search_query = """
-    
+
         SELECT keyword, RANK() OVER (ORDER BY MAX(created_datetime) DESC, COUNT(*) DESC) AS search_rank
 
         FROM search_log_keywords
+        WHERE keyword IN (
+            SELECT DISTINCT item
+            FROM (
+                SELECT name_org as item FROM artist
+                UNION ALL
+                SELECT name_org as item FROM track
+                UNION ALL
+                SELECT name_org as item FROM album
+            ) AS items
+            WHERE item IS NOT NULL
+        )
         GROUP BY keyword
         ORDER BY search_rank;
 
     """
 
-    value_check_query = """
-        SELECT item
-        FROM (
-            SELECT name_org as item FROM artist
-            UNION ALL
-            SELECT name_org as item FROM track
-            UNION ALL
-            SELECT name_org as item FROM album
-        ) AS items
-        WHERE item IS NOT NULL
-        AND item = %s;
-    """
-
+    
     cursor.execute(search_query)
     search_ranking = cursor.fetchall()
 
     result = {}
-    prev_search_rank = None
-    rank = 0
+    rank = 1
     
     for _, (keyword, search_rank) in enumerate(search_ranking):
-        cursor.execute(value_check_query, (keyword,))
-        if cursor.fetchone():
-            if search_rank != prev_search_rank:  # 동일한 순위가 아니면 순위 업데이트
-                rank += 1
         result[rank] = keyword
-        prev_search_rank = search_rank
+        rank += 1
             
         if rank >= 20:  # 20위까지만 결과 저장
             break
-
-            result[rank] = keyword
-            prev_search_rank = search_rank
-            
-            if rank >= 10:  # 10위까지만 결과 저장
-                break
-
-    
     connection.close()
     return result
 
