e614fd67502e8f412238d21ddb70e62db577e72f
bksooon <sbka0704@nate.com>
2023-09-12 15:32:56 +0900

    230912/bkson/integrated_chart genie 정규화 orm 수정

--- src/load_chart_genie.py
+++ src/load_chart_genie.py
@@ -5,15 +5,10 @@ root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
 sys.path.append(root_path)
 
 from update_token import return_token
-from model.chart_genie import ChartGenie #, ChartGenieORM
+from model.chart_genie import ChartGenie , ChartGenieORM
 from model.database import session_scope
 
 access_token = return_token()
-
-def process_sp_json(sp_json):
-    for e in sp_json:
-        entity = ChartGenie(**e)
-        print(entity)
 
 if __name__ == '__main__' :
     response = requests.post('https://app.genie.co.kr/chart/j_RealTimeRankSongList.json'
@@ -25,11 +20,25 @@ if __name__ == '__main__' :
                              }
                             )
 
+    def process_sp_json(sp_json):
+        for e in sp_json:
+            entity = ChartGenie(**e)
+            orm = ChartGenieORM(entity)
+            print(orm)
+            with session_scope() as session :
+                  session.add(orm)
+             
+        else : print(response.status_code)
+
     if response.status_code == 200 :
         responsed_data = response.json().get('DataSet').get('DATA')
+        
+        song_name = []
+        artist_name = []
+        album_name = []
+        album_img = []
 
         entries = {}
-
         for index, item in enumerate(responsed_data):
             # 제목 디코딩
             pre_track_title = item['SONG_NAME']
@@ -46,8 +55,7 @@ if __name__ == '__main__' :
             entries[index] = [track_title, artists, album]
         
         for i in range(len(responsed_data)):
-            q = entries[i][0]  + " " + entries[i][2]
-            print(q)
+            q = entries[i][0]  + " " + entries[i][1]
             url = f'https://api.spotify.com/v1/search?q={q}&type=track&limit=1'
             headers = {
                 'Authorization': 'Bearer '+access_token
@@ -55,10 +63,21 @@ if __name__ == '__main__' :
             response_sp = requests.get(url, headers=headers)
             if response_sp.status_code == 200:
                 sp_json = response_sp.json().get('tracks').get('items')
-                process_sp_json(sp_json)
                 
-            elif response_sp.status_code == 400 :
-                q = entries[i][0] +  " " + entries[i][2] +  " " + entries[i][3]
+                artists_sp = []
+                
+                process_sp_json(sp_json)
+                song_name.append(sp_json[0]['name'])
+                album_name.append(sp_json[0]['album']['name'])
+                album_img.append(sp_json[0]['album']['images'][0]['url'])
+            
+                for j in range(len(sp_json[0]['artists'])):
+                    artists_sp.append(sp_json[0]['artists'][j]['name'])
+                artist_name.append(', '.join(artists_sp))
+                
+                
+            elif response_sp.status_code != 200 :
+                q = entries[i][0] +  " " + entries[i][1] +  " " + entries[i][2]
                 url = f'https://api.spotify.com/v1/search?q={q}&type=track&limit=1'
                 headers = {
                     'Authorization': 'Bearer '+access_token
@@ -67,81 +86,25 @@ if __name__ == '__main__' :
                 
                 if response_sp.status_code == 200:
                     sp_json = response_sp.json().get('tracks').get('items')
-                    process_sp_json(sp_json)
+                    artists_sp = []
+                    song_name.append(sp_json[0]['name'])
+                    album_name.append(sp_json[0]['album']['name'])
+                    album_img.append(sp_json[0]['album']['images'][0]['url'])
                     
-                ###############################################################
-                # Done: name, albumname, imagesUrl, artist
-                
-                # print(type(sp_json[0]['artists'])) # list
-                # print(type(sp_json[0]['artists'][0])) # dict
-                # print(type(sp_json[0]['artists'][0]['name'])) # str
-                # print(type(sp_json[0]['album']['images'][0]['url'])) # dict
-                
-                ###############################################################
-                
-                # for j in range(len(sp_json['tracks']['items'][0]['artists'])):
-                #     artists_sp.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
-            
-                #     artist_name.append(artists_sp)
-                
-                
-                
-
-                
-                # print(data_type_counts)
-                    # print(e)
-                    # entity = ChartGenie(**e)
-                
-        #         artists_sp = []
-
-        #         song_name.append(sp_json['tracks']['items'][0]['name'])
-        #         album_name.append(sp_json['tracks']['items'][0]['album']['name'])
-        #         album_img.append(sp_json['tracks']['items'][0]['album']['images'][0]['url'])
-                
-        #         for j in range(len(sp_json['tracks']['items'][0]['artists'])):
-        #             artists_sp.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
-                    
-        #         artist_name.append(artists_sp)
-                
-        #     elif response_sp.status_code != 200 :
-        #         q = entries[i][0] + " " + entries[i][1]
-        #         url = f'https://api.spotify.com/v1/search?q={q}&type=track&limit=1'
-        #         headers = {
-        #             'Authorization': 'Bearer '+access_token
-        #         }
-        #         response_sp = requests.get(url, headers=headers)
-                
-        #         if response_sp.status_code == 200:
-        #             sp_json = response_sp.json()
-        #             artists_sp = []
-                    
-        #             song_name.append(sp_json['tracks']['items'][0]['name'])
-                    
-        #             album_name.append(sp_json['tracks']['items'][0]['album']['name'])
-                    
-        #             album_img.append(sp_json['tracks']['items'][0]['album']['images'][0]['url'])
-                    
-        #             for j in range(len(sp_json['tracks']['items'][0]['artists'])):
-        #                 artists_sp.append(sp_json['tracks']['items'][0]['artists'][j]['name'])
-                        
-        #             artist_name.append(', '.join(artists_sp))
-                
-                
-        #         else: print(f'{i} : {response_sp.status_code}')
-
-        # print(f"곡 : {(song_name)}   가수 : {(artist_name)}    앨범 : {(album_name)}")
-        # for i in range(len(responsed_data)):
-        #     responsed_data[i]['SONG_NAME'] = song_name[i]
-        #     responsed_data[i]['ARTIST_NAME'] = artist_name[i]
-        #     responsed_data[i]['ALBUM_NAME'] = album_name[i]
-        #     responsed_data[i]['ALBUM_IMG_PATH'] = album_img[i]
+                    for j in range(len(sp_json[0]['artists'])):
+                        artists_sp.append(sp_json[0]['artists'][j]['name'])
+                    artist_name.append(', '.join(artists_sp))
         
-        # print(responsed_data)
-        # for e in responsed_data :
-            # entity = ChartGenie(**e)
-            # orm = ChartGenieORM(entity)
-# 
-            # with session_scope() as session :
-                # session.add(orm)
-# 
-    # else : print(response.status_code)
+            responsed_data[i]['SONG_NAME'] = song_name[i]
+            responsed_data[i]['ARTIST_NAME'] = artist_name.pop()
+            responsed_data[i]['ALBUM_NAME'] = album_name[i]
+            responsed_data[i]['ALBUM_IMG_PATH'] = album_img[i]
+            
+            for e in responsed_data :
+                entity = ChartGenie(**e)
+                orm = ChartGenieORM(entity)
+
+                with session_scope() as session :
+                    session.add(orm)
+
+            else : print(response.status_code)
