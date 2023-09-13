68a5cf6170056f7e9adccd6bdd4b12e8b8a23107
iamyanghee <yanghee.data@gmail.com>
2023-09-13 12:17:08 +0900

    orm 모델 완성 및 적재 이상무 확인 완료

--- model/audio_features.py
+++ model/audio_features.py
@@ -5,7 +5,7 @@ sys.path.append(root_path)
 
 from model.database import Base
 from sqlalchemy.sql.schema import Column
-from sqlalchemy import String, Integer, ARRAY
+from sqlalchemy import String, Integer, ARRAY, Float
 from pydantic import BaseModel, ConfigDict
 from typing import Dict, List, Union, Optional
 
@@ -26,6 +26,24 @@ class SpotifyAudioFeaturesORM(Base):
     __tablename__ = 'spotify_audio_features'
 
     id = Column(String, primary_key=True)
+    acousticness = Column(Float, default=0)
+    danceability = Column(Float, default=0)
+    energy = Column(Float, default=0)
+    instrumentalness = Column(Float, default=0)
+    liveness = Column(Float, default=0)
+    loudness = Column(Float, default=0)
+    speechiness = Column(Float, default=0)
+    tempo = Column(Float, default=0)
+    valence = Column(Float, default=0)
 
-    def __init__(self, audio_features:SpotifyAudioFeatures) :
-        self.id = audio_features.id
+    def __init__(self, audio:SpotifyAudioFeatures) :
+        self.id = audio.id
+        self.acousticness = audio.acousticness
+        self.danceability = audio.danceability
+        self.energy = audio.energy
+        self.instrumentalness = audio.instrumentalness
+        self.liveness = audio.liveness
+        self.loudness = audio.loudness
+        self.speechiness = audio.speechiness
+        self.tempo = audio.tempo
+        self.valence = audio.valence
