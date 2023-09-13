91e85aa3d1afd253b5341a72cfb1f03bbfe8221a
iamyanghee <yanghee.data@gmail.com>
2023-09-13 14:23:09 +0900

    spotify api /audio-features/{id} 으로 적재 기능 구현 완료

--- src/get_audio_features.py
+++ src/get_audio_features.py
@@ -8,25 +8,39 @@ import model.audio_features as Audio
 
 import requests
 
+def get_audio_features(id:str) :
+    response = requests.get('https://api.spotify.com/v1/audio-features/'
+                            +f'{id}'
+                        ,headers={
+                            'Authorization': 'Bearer '+ access_token
+                        }
+                        )
+    if response.status_code == 200 :
+        responsed_data = response.json()
+        parsed_data = Audio.SpotifyAudioFeatures(**responsed_data)
+        return parsed_data
+
+def load_audio_features(data:Audio.SpotifyAudioFeatures):
+    orm = Audio.SpotifyAudioFeaturesORM(data)
+    with session_scope() as session :
+        session.add(orm)
+
 if __name__ == '__main__' :
 
+    # 0 - get spotify token
     from src.get_token import update_token, return_token
     access_token = return_token()
     # access_token = update_token('iamsophie')
 
-    response = requests.get('https://api.spotify.com/v1/audio-features/'
-                 +'02SbQgZbzMoylPoGr32ugF'
-            ,headers={
-                'Authorization': 'Bearer '+ access_token
-            }
-            )
-    if response.status_code == 200 :
-        responsed_data = response.json()
-        
-        parsed_data = Audio.SpotifyAudioFeatures(**responsed_data)
-        orm = Audio.SpotifyAudioFeaturesORM(parsed_data)
-        
-        with session_scope() as session :
-            session.add(orm)
+    # 1 - get spotify audio features by track id
+    spotify_track_id = '02SbQgZbzMoylPoGr32ugF'
+    parsed_data = get_audio_features(spotify_track_id)
 
-    else : print(response.status_code)
+    # 2 - load db : spotify_audio_features 
+    load_audio_features(parsed_data)
+
+    # 2 확인
+    with session_scope() as session :
+        result = session.query(Audio.SpotifyAudioFeaturesORM).filter_by(id='02SbQgZbzMoylPoGr32ugF').one()
+        print(type(result))
+        print(result.acousticness)
