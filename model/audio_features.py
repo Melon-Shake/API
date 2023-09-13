3a5efcabda6d9329ac7be87c6d77c78950ec81d1
iamyanghee <yanghee.data@gmail.com>
2023-09-13 12:02:17 +0900

    spotify api /audio-features 리턴값 받을 pydantic 모델 추가

--- model/audio_features.py
+++ model/audio_features.py
@@ -8,3 +8,15 @@ from sqlalchemy.sql.schema import Column
 from sqlalchemy import String, Integer, ARRAY
 from pydantic import BaseModel, ConfigDict
 from typing import Dict, List, Union, Optional
+
+class SpotifyAudioFeatures(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    acousticness: float
+    danceability: float
+    energy: float
+    instrumentalness: float
+    liveness: float
+    loudness: float
+    speechiness: float
+    tempo: float
+    valence: float
