9654331b3bdc0c795523a1f4af36d5791b4eaa5a
iamyanghee <yanghee.data@gmail.com>
2023-09-13 10:32:03 +0900

    기존 lyrics 테이블은 두고, lyrics_temp 으로 새로 테이블 설정

--- sql/lyrics.sql
+++ sql/lyrics.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS lyrics (
+CREATE TABLE IF NOT EXISTS lyrics_temp (
     spotify_tracks_id varchar(255),
 	content TEXT NOT NULL,
+    musix_match boolean,
+    genius boolean,
 	created_datetime timestamp DEFAULT current_timestamp,
 	modified_datetime timestamp DEFAULT current_timestamp
 )
