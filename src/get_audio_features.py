c558473af0f7f0e1cdf2c00bdee0274253dcdde1
iamyanghee <yanghee.data@gmail.com>
2023-09-13 12:03:37 +0900

    spotify api /audio-features 리턴받아 파싱까지 확인 완료

--- src/get_audio_features.py
+++ src/get_audio_features.py
@@ -7,3 +7,23 @@ from model.database import session_scope
 import model.audio_features as Audio
 
 import requests
+
+if __name__ == '__main__' :
+
+    from src.get_token import update_token, return_token
+    access_token = return_token()
+    # access_token = update_token('iamsophie')
+
+    response = requests.get('https://api.spotify.com/v1/audio-features/'
+                 +'02SbQgZbzMoylPoGr32ugF'
+            ,headers={
+                'Authorization': 'Bearer '+ access_token
+            }
+            )
+    if response.status_code == 200 :
+        responsed_data = response.json()
+        
+        parsed_data = Audio.SpotifyAudioFeatures(**responsed_data)
+        print(parsed_data.acousticness)
+
+    else : print(response.status_code)
