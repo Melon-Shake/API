14d746e4bbda3fbd6593c38976d9782faaeb7c5b
mews2000 <mews2000@gmail.com>
2023-09-14 11:01:53 +0900

    v0.2.0-bug-fix

--- src/TotalApi.py
+++ src/TotalApi.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONRespond
+from fastapi.responses import JSONResponse
+import sys
+import os
+root_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
+sys.path.append(root_path)
+import requests, pandas as pd
 import lib.module as module
 from pydantic import BaseModel
 import bcrypt
