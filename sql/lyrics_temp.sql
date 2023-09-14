a7e63ed61ce91637c627fe2a073967e6755a84cf
iamyanghee <131243101+iamyanghee@users.noreply.github.com>
2023-09-14 11:12:48 +0900

    lyrics_temp 테이블에 4가지 word 카운트하는 컬럼 추가 (#111)
    브랜치 헷갈려서 병합

--- sql/lyrics_temp.sql
+++ sql/lyrics_temp.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS lyrics_temp (
     spotify_tracks_id varchar(255),
 	content TEXT NOT NULL,
+    romantic_words integer,
+    adventurous_words integer,
+    powerful_words integer,
+    depressed_words integer,
     musix_match boolean,
     genius boolean,
 	created_datetime timestamp DEFAULT current_timestamp,
