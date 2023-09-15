6634383087d9fdd5e15a5a8b309cbe017036ebf0
jungssg <younwjdtjr@naver.com>
2023-09-15 14:18:27 +0900

    test 파일 제거

--- src/TotalApi.py
+++ src/TotalApi.py
@@ -26,8 +26,6 @@ from model.jun_model import *
 from get_keyword import save_keyword_data
 from user_data import user_data
 from user_search_track import pick_data
-from daily_search_ranking import daily_search_ranking
-from integrate_chart_test import generate_total_chart
 # import sys, numpy as np, pandas as pd, json, requests, re
 
 
