4e0423d81c2b5951da20f0cb1be000b494c19c14
jungssg <younwjdtjr@naver.com>
2023-09-15 16:39:23 +0900

    table fix

--- sql/chart_vibe.sql
+++ sql/chart_vibe.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS chart_vibe (
 	id serial,
   track_name varchar(255),
-  artist_names varchar(255) [],
+  artist_names varchar(255),
   album_name varchar(255),
   img_url varchar(255),
   release_date varchar(255),
